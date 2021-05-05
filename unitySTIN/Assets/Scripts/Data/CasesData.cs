ee87628799755ea9653fa97984e705e24c0e9d3e
aliaksei.kalosha <aliaksei.kalosha@tul.cz>
2021-05-05 21:42:56 +0200

    add data types

--- unitySTIN/Assets/Scripts/Data/CasesData.cs
+++ unitySTIN/Assets/Scripts/Data/CasesData.cs
@@ -1,3 +1,13 @@
-﻿public class CasesData
+﻿using System;
+public class CasesData
 {
+    public StateCaseData Mzcr = null;
+    public StateCaseData Who = null;
+    public DateTime day;
+}
+public class StateCaseData
+{
+    public int PerDay = 0;
+    public int Total = 0;
+    public DateTime updated;
 }
--- unitySTIN/Assets/Scripts/Data/VacinationData.cs
+++ unitySTIN/Assets/Scripts/Data/VacinationData.cs
@@ -1,3 +1,13 @@
-﻿public class VacinationData
+﻿using System;
+using System.Collections.Generic;
+public class VacinationData
 {
+    public List<VacinationStateData> data = new List<VacinationStateData>();
+    public DateTime day;
+}
+public class VacinationStateData
+{
+    public float Percent = 0;
+    public int Population = 0;
+    public string StateName = "";
 }
